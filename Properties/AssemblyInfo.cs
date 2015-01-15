a3d09cc514080f89b23e04e2fd36b6ed3ec2bbb7
izrik <izrik@izrik.com>
2015-01-15 12:22:51 -0600

    Initial project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("MetaphysicsIndustries.Giza.Cpp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Metaphysics Industries, Inc.")]
+[assembly: AssemblyProduct("MetaphysicsIndustries.Giza.Cpp")]
+[assembly: AssemblyCopyright("Copyright 2015 Metaphysics Industries, Inc.")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("0.1.*")]
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
